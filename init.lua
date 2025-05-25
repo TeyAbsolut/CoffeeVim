776753e3b239502e8d5603cc0542ffc54f0d28ad
TeyAbsolut <oussama.hajji.ing@gmail.com>
2025-05-25 05:55:12 +0200

    renamed startup to splash

--- init.lua
+++ init.lua
@@ -3,6 +3,6 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require('config.lazy')
-require('config.startup')
+require('config.splash')
 require('config.setup')
 require('config.keymaps')

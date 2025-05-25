020f6b84af3c96a0b3bc9f65b63a1600c020b5ad
TeyAbsolut <oussama.hajji.ing@gmail.com>
2025-05-25 18:46:46 +0200

    working vue support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,61 +1,71 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		-- Lua configuration
-		vim.lsp.config("lua_ls", {
-			on_init = function(client)
-				if client.workspace_folders then
-					local path = client.workspace_folders[1].name
-					if
-						path ~= vim.fn.stdpath("config")
-						and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-					then
-						return
-					end
-				end
-				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-					runtime = {
-						-- Tell the language server which version of Lua you're using (most
-						-- likely LuaJIT in the case of Neovim)
-						version = "LuaJIT",
-						-- Tell the language server how to find Lua modules same way as Neovim
-						path = {
-							"lua/?.lua",
-							"lua/?/init.lua",
-						},
-					},
+    "neovim/nvim-lspconfig",
+    config = function()
+        -- Lua configuration
+        vim.lsp.config("lua_ls", {
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if
+                        path ~= vim.fn.stdpath("config")
+                        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+                    then
+                        return
+                    end
+                end
+                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using (most
+                        -- likely LuaJIT in the case of Neovim)
+                        version = "LuaJIT",
+                        -- Tell the language server how to find Lua modules same way as Neovim
+                        path = {
+                            "lua/?.lua",
+                            "lua/?/init.lua",
+                        },
+                    },
 
-					diagnostics = {
-						globals = {
-							"vim",
-						},
-					},
-					-- Make the server aware of Neovim runtime files
-					workspace = {
-						checkThirdParty = false,
-						library = {
-							vim.env.VIMRUNTIME,
-						},
-					},
-				})
-			end,
-			settings = {
-				Lua = {},
-			},
-		})
+                    diagnostics = {
+                        globals = {
+                            "vim",
+                        },
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                        },
+                    },
+                })
+            end,
+            settings = {
+                Lua = {},
+            },
+        })
 
-		local lspconfig = require("lspconfig")
+        local lspconfig = require("lspconfig")
+        local home = os.getenv("HOME")
 
-		lspconfig.ts_ls.setup({
-			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-		})
+        lspconfig.ts_ls.setup({
+            init_options = {
+                plugins = { -- I think this was my breakthrough that made it work
+                    {
+                        name = "@vue/typescript-plugin",
+                        location = home .. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                        languages = { "vue" },
+                    },
+                },
+            },
+            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        })
 
-		local servers = { "css_variables", "cssmodules_ls", "cssls", "angularls", "svelte" }
+        local servers = { "css_variables", "cssmodules_ls", "cssls", "angularls", "svelte" }
 
-		for _, lsp in ipairs(servers) do
-			lspconfig[lsp].setup({})
-		end
+        for _, lsp in ipairs(servers) do
+            lspconfig[lsp].setup({})
+        end
 
-		lspconfig.vls.setup({})
-	end,
+        lspconfig.volar.setup({})
+    end,
 }
